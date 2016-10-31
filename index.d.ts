2c6db414d358ae727836c87a67c022ea489c77ab
Dawson Reid <dreid93@gmail.com>
2016-10-31 12:12:23 -0300

    I have added type definitions for AdGroups. The API exposed is large any many methods return or accept any because I have not yet fully defined the definitions.

--- index.d.ts
+++ index.d.ts
@@ -17,9 +17,15 @@ declare namespace AdWordsScripts {
     withIds(ids: number[]): AdWordsBasicSelector<T>;
   }
 
+  type AdWordsDateObject = {
+    year: string,
+    month: string,
+    day: string
+  };
+
   interface AdWordsSelector<T extends AdWordsEntity> {
     forDateRange(dateRande: string): AdWordsSelector<T>;
-    forDateRange(dateFrom: string, dateTo: string): AdWordsSelector<T>;
+    forDateRange(dateFrom: string | AdWordsDateObject, dateTo: string | AdWordsDateObject): AdWordsSelector<T>;
     get(): AdWordsIterator<T>;
     orderBy(orderBy: string): AdWordsSelector<T>;
     withCondition(condition: string): AdWordsSelector<T>;
@@ -56,7 +62,117 @@ declare namespace AdWordsScripts {
       rows(): ReportRowIterator;
     }
 
+    interface OwnedByCampaign {
+      getCampaign(): Campaign;
+    }
+
+    interface OwnedByAdGroup {
+      getAdGroup(): AdGroup;
+    }
+
+    interface Keyword extends AdWordsEntity {
+      // TODO : Complete the Keyword interface
+    }
+
+    interface AdParam extends AdWordsEntity {
+      getAdGroup(): AdGroup;
+      getIndex(): number;
+      getInsertionText(): string;
+      getKeyword(): Keyword;
+      remove(): void;
+      setInsertionText(insertionText: string): void;
+    }
+
+    interface AdParamSelector {
+      get(): AdWordsIterator<AdParam>;
+    }
+
+    interface Ad extends AdWordsEntity {
+
+    }
+
+    interface AdGroup extends AdWordsEntity {
+      adParams(): AdParamSelector;
+
+      // FIXME : This should take a CalloutExtension and return a CalloutOperation
+      addCallout(calloutExtension: any): any;
+      // FIXME : This should take a MobileAppExtension and return a MobileApp
+      addMobileApp(mobileAppExtension: any): any;
+      // FIXME : This should take a PhoneNumberExtension and return a PhoneNumber
+      addPhoneNumber(phoneNumberExtension: any): any;
+      // FIXME : This should take a ReviewExtension and return a Review
+      addReview(reviewExtension: any): any;
+      // FIXME : This should take a SitelinkExtension and return a SitelinkOperation
+      addSitelink(sitelinkExtension: any): any;
+
+      ads(): AdWordsSelector<Ad>;
+      applyLabel(label: string): void;
+
+      // FIXME : This should return AdGroupBidding
+      bidding(): any;
+
+      createNegativeKeyword(keywordText): void;
+
+      // FIXME : This should return AdGroupDevices
+      devices(): any;
+      // FIXME : This should return AdGroupDisplay
+      display(): any;
+
+      enable(): void;
+
+      // FIXME : This should return AdGroupExtensions
+      extensions(): any;
+
+      getCampaign(): Campaign;
+      getId(): number;
+      getName(): string;
+
+      // FIXME : These should return Stats
+      getStatsFor(dateRange: string): any;
+      getStatsFor(dateFrom: string | AdWordsDateObject, dateTo: string | AdWordsDateObject): any;
+
+      isEnabled(): boolean;
+      isPaused(): boolean;
+      isRemoved(): boolean;
+      keywords(): AdWordsSelector<Keyword>;
+
+      // FIXME : This should return a AdWordsSelector<NegativeKeyword>
+      negativeKeywords(): any;
+      // FIXME : This should return an AdBuilderSpace
+      newAd(): any;
+      // FIXME: This should return a KeywordBuilder
+      newKeywordBuilder(): any;
+
+      pause(): void;
+
+      // FIXME : This should take a CalloutExtension and return a Callout
+      removeCallout(calloutExtension: any): any;
+
+      removeLabel(label: string): void;
+
+      // FIXME : This should take a MobileAppExtension and return a MobileApp
+      removeMobileApp(mobileAppExtension: any): any;
+      // FIXME : This should take a PhoneNumberExtension and return a PhoneNumber
+      removePhoneNumber(phoneNumberExtension: any): any;
+      // FIXME : This should take a ReviewExtension and return a Review
+      removeReview(reviewExtension: any): any;
+      // FIXME : This should take a SitelinkExtension and return a SitelinkOperation
+      removeSitelink(sitelinkExtension: any): any;
+
+      setName(name: string): void;
+
+      // FIXME : This should return AdGroupTargeting
+      targeting(): any;
+      // FIXME : This should return AdGroupUrls
+      urls(): any;
+    }
+
+    interface Campaign extends AdWordsEntity {
+      adGroups(): AdWordsSelector<AdGroup>;
+    }
+
     interface AdWordsApp {
+      campaigns(): AdWordsSelector<Campaign>;
       report(query, options?: ReportQueryOptions): Report;
     }
   }
